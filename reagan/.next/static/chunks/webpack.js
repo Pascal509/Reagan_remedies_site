42d9c5e9d7f924e3796e00b6bd96ead2a8dcb53a
Ezenagu Chinemerem Paschal <chinemeremezenagu@gmail.com>
2026-02-22 20:51:13 +0100

    remove .next folder and update gitignore

--- reagan/.next/static/chunks/webpack.js
+++ reagan/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "859a50f51c7ddae8"; }
+/******/ 		__webpack_require__.h = function() { return "af6a8349d557ce6c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
